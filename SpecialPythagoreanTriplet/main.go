0238b3ed98f93859426fe559656f23531fd21e47
OoiBP <bpooi0702@gmail.com>
2017-04-18 18:26:37 +0800

    added Largest product grid

--- SpecialPythagoreanTriplet/main.go
+++ SpecialPythagoreanTriplet/main.go
@@ -16,3 +16,9 @@ func main() {
 		}
 	}
 }
+
+// A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+// a2 + b2 = c2
+// For example, 32 + 42 = 9 + 16 = 25 = 52.
+// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+// Find the product abc.
--- SummationOfPrimes/main.go
+++ SummationOfPrimes/main.go
@@ -33,3 +33,6 @@ func main() {
 	}
 	fmt.Println(total)
 }
+
+// The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+// Find the sum of all the primes below two million.
